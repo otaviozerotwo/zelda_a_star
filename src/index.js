aea69723ceb19e29b15045daddf7091a45e75e94
Otavio Cardoso <otavio.cardoso@rfn.ptc>
2024-03-14 11:22:36 -0300

    refactor: implementação roteamento para navegar entre as telas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainMap from './components/screens/MainMap';
 import DungeonOne from './components/screens/DungeonOne';
 import DungeonTwo from './components/screens/DungeonTwo';
 import DungeonThree from './components/screens/DungeonThree';
+import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <MainMap />
-  </StrictMode>
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<MainMap />} />
+      <Route path="/dungeonOne" element={<DungeonOne />} />
+      <Route path="/dungeonTwo" element={<DungeonTwo />} />
+      <Route path="/dungeonThree" element={<DungeonThree />} />
+    </Routes>
+  </Router>
 );
 
